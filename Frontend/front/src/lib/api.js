220ec80c0e3dc4ee99e891572c89a261b1c56616
Juan Miguel Muñoz Puentes <143244902+jumunozdev@users.noreply.github.com>
2025-03-21 17:46:01 -0500

    Update api.js

--- Frontend/front/src/lib/api.js
+++ Frontend/front/src/lib/api.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8081/productos',
+  baseURL: process.env.NEXT_PUBLIC_API_URL || 'http://backend-service:8081/productos',
 });
