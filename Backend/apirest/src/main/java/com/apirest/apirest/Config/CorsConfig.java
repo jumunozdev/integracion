c1603727a29021d785e16f2dfeebdcf7da85973c
Juan Miguel Muñoz Puentes <143244902+jumunozdev@users.noreply.github.com>
2025-03-21 20:10:29 -0500

    Update CorsConfig.java

--- Backend/apirest/src/main/java/com/apirest/apirest/Config/CorsConfig.java
+++ Backend/apirest/src/main/java/com/apirest/apirest/Config/CorsConfig.java
@@ -13,7 +13,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:8000") // Permitir el frontend
+                        .allowedOrigins("http://localhost:8080") // Permitir el frontend
                         .allowedMethods("GET", "POST", "PUT", "DELETE") // Métodos permitidos
                         .allowCredentials(true);
             }
